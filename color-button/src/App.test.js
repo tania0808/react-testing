7150640ff9160dc6537e2f50e3121da1d8d10ff6
Tania <tania@MacBook-Pro-de-nico.local>
2022-09-30 10:47:38 +0200

    Test and code function to transform camel case colors to spaced colors

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -5,7 +5,7 @@ import { replaceCamelWithSpaces } from './App'
 test('button has correct initial color', () => {
   render(<App/>);
 
-  // find an element with a role of buttoj and text of 'Change to blue'
+  // find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
   // expect the background to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
